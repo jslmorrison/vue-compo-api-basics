4fa385c7c5be4965e09498c90615046e1ecaca79
John Morrison <jslmorrison@gmail.com>
2023-09-30 16:48:44 +0100

    Add another ref
    To enable editing of h4 element value (data bining).

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="home">
+    <h3>{{ counterTitle }}:</h3>
     <button @click="decreaseCounter" class="btn">-</button>
     <span class="counter">{{ counter }}</span>
     <button @click="increaseCounter" class="btn">+</button>
+    <div class="edit">
+      <h4>Edit counter title</h4>
+      <input v-model="counterTitle" type="text" />
+    </div>
   </div>
+
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
   const counter = ref(0)
+  const counterTitle = ref('My Counter')
 
   const increaseCounter = () => {
     counter.value++
@@ -28,5 +35,8 @@ import { ref } from 'vue'
 .btn, .counter {
   font-size: 40px;
   margin: 10px;
+}
+.edit {
+  margin-top: 60px;
 }
 </style>
