75e35b0568d077fcec13ef4fd608c44185336242
John Morrison <jslmorrison@gmail.com>
2023-09-30 16:22:39 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <button class="btn">-</button>
+    <span class="counter">{{ counter }}</span>
+    <button class="btn">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 1
+    }
+  }
+}
+</script>
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
