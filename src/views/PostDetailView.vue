59a68c1c6d607d0ec6ad987d047a6b6516da060a
John Morrison <jslmorrison@gmail.com>
2023-10-05 19:29:42 +0100

    Add 'useRoute' functionality

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -2,6 +2,19 @@
   <div class="post-detail">
     <h1>Post detail</h1>
     <p>Post with id of {{ $route.params.id }}</p>
+    <div>
+      <button @click="showPostId">Show post id</button>
+    </div>
     <p><RouterLink to="/posts">&lt; Go back</RouterLink></p>
   </div>
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+
+const showPostId = () => {
+  alert(`ID of post is: ${ route.params.id }`);
+}
+</script>
