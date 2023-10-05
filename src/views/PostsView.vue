4b234496b8680a021fe690ff3264b6d8a64f876a
John Morrison <jslmorrison@gmail.com>
2023-10-05 19:43:44 +0100

    Change list of posts to loop over a ref object

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -3,19 +3,33 @@
     <h1>Posts</h1>
 
     <ul>
-      <li>
-        <RouterLink to="/post/id1">Post 1</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/post/id2">Post 2</RouterLink>
-      </li>
-      <li>
-        <RouterLink to="/post/id3">Post 3</RouterLink>
+      <li v-for="post in posts" :key="post.id">
+        <RouterLink :to="`/post/${ post.id }`">{{ post.title }}</RouterLink>
       </li>
     </ul>
   </div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
+
+
+const posts = ref([
+  {
+    id: 1,
+    title: 'Post 1'
+  },
+  {
+    id: 2,
+    title: 'Post 2'
+  },
+  {
+    id: 3,
+    title: 'Post 3'
+  },
+])
+
+</script>
 <style scoped>
 li {
   margin-bottom: 20px;
