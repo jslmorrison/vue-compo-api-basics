5e326d3eb38ae8c05823d2ac1f7e138e342497f5
John Morrison <jslmorrison@gmail.com>
2023-10-05 19:24:38 +0100

    Add links and update detail view

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="post-detail">
     <h1>Post detail</h1>
+    <p>Post with id of {{ $route.params.id }}</p>
+    <p><RouterLink to="/posts">&lt; Go back</RouterLink></p>
   </div>
 </template>
