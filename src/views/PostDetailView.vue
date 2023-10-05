acad580ed5dd875b1369cf94f5cd4469b44c79ee
John Morrison <jslmorrison@gmail.com>
2023-10-05 19:34:38 +0100

    Add 'useRouter' functionality

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -5,16 +5,26 @@
     <div>
       <button @click="showPostId">Show post id</button>
     </div>
+    <div>
+      <button @click="goHomein3Secs">Go home in 3 secs</button>
+    </div>
     <p><RouterLink to="/posts">&lt; Go back</RouterLink></p>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute()
+const router = useRouter()
 
 const showPostId = () => {
   alert(`ID of post is: ${ route.params.id }`);
+}
+
+const goHomein3Secs = () => {
+  setTimeout(() => {
+    router.push({ name: 'home' })
+  }, 3000)
 }
 </script>
