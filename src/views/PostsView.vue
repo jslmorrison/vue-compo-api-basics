2a01ba1ad06ed07a4106cf4e0a04bdf3ac2b9260
John Morrison <jslmorrison@gmail.com>
2024-01-20 17:27:24 +0000

    Use composable in posts view

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -4,34 +4,43 @@
 
     <ul>
       <li v-for="post in posts" :key="post.id">
-        <RouterLink :to="`/post/${ post.id }`">{{ post.title }}</RouterLink>
+        <RouterLink :to="`/post/${post.id}`">{{ post.title }}</RouterLink>
       </li>
     </ul>
+
+    <button class="counter-button" @click="increaseCounter(1)">{{ counterData.count }}</button>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-
+import { ref } from 'vue'
+import { useCounter } from '@/use/useCounter'
 
 const posts = ref([
   {
     id: 1,
-    title: 'Post 1'
+    title: "Post 1",
   },
   {
     id: 2,
-    title: 'Post 2'
+    title: "Post 2",
   },
   {
     id: 3,
-    title: 'Post 3'
+    title: "Post 3",
   },
-])
+]);
 
+const { increaseCounter, counterData } = useCounter()
 </script>
+
 <style scoped>
 li {
   margin-bottom: 20px;
+}
+.counter-button {
+  background-color: red;
+  padding: 10px;
+  width: 100%;
 }
 </style>
